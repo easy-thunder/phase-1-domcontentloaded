cbad3caadf107527500d33499a487a7278f62916
Jacob David Diehl <jakediehl17@gmail.com>
2022-12-14 15:40:53 -0700

    commit tree f0f2c642f8467448689bd81e9d3aaa11c58c45eb

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
 // Your code goes here
+'use strict'
+document.addEventListener('DOMContentLoaded', function(){
+
+console.log("The DOM has loaded");
+
+
+const text = document.querySelector('#text');
+text.textContent = "This is really cool!"
+
+console.log(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
